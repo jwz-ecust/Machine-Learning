d8cddea5b2aa8ac57f7257560f17b7cde895d2bf
jwz-ecust <aaronzjw6@gmail.com>
2016-10-30 23:34:53 +0800

    zz

--- code/Pybrain/knn/file2matrix.py
+++ code/Pybrain/knn/file2matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def file2matrix(filename):
@@ -14,13 +15,19 @@ def file2matrix(filename):
     for line in arrayoline:
         line = line.strip()
         listfromline = line.split('\t')
+        print listfromline
         returnmatrix[index, :] = listfromline[0:3]
         classlabervector.append(listfromline[-1])
-    index += 1
+        index += 1
     return returnmatrix, classlabervector
 
 
 filepath = "E:/Dropbox/Machine-Learning/code/Pybrain/knn/datingTestSet.txt"
 matrix, label = file2matrix(filepath)
 print matrix
-print label
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+ax1.scatter(matrix[:, 1], matrix[:, 2])
+plt.savefig('E:/Dropbox/Machine-Learning/code/Pybrain/knn/scatter.jpg')
+plt.show()
